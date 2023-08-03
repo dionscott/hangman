31b8de145315ae426a0a001ddb62798752718429
Dion Scott <dionanthonyscott@gmail.com>
2023-08-03 11:57:54 -0500

    Create classes to hold functions

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,17 +1,31 @@
-word_list = File.read('wordsheet.txt')
-word_list
-
-# make into an array seperate by /n
-# find words that are 5 - 12 in length
-# make that the array to sample from
-# initialize game with that
-list = word_list.split("\n")
-def check_length(word)
-    length = word.length
-    if length >= 5 && length <= 12
-        true
-    else
-        false
+class Player
+    def initialize
     end
 end
-p valid_words = list.select { |word| check_length(word) }
+
+class Display
+    def initialize
+        puts "Let's play Hangman!"
+    end
+end
+
+class Game
+    def initialize
+        word_list = File.read('wordsheet.txt')
+        list = word_list.split("\n")
+        @valid_words = list.select { |word| check_length(word) }
+        @display = Display.new
+        @player = Player.new
+    end
+    
+    def check_length(word)
+        length = word.length
+        if length >= 5 && length <= 12
+            true
+        else
+            false
+        end
+    end
+end
+
+game = Game.new
