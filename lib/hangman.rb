de1e3debaf89e3887fe83dd363c69890b96c69b3
Dion Scott <dionanthonyscott@gmail.com>
2023-08-03 11:50:27 -0500

    Find words that are between 5 and 12 in length

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,2 +1,17 @@
 word_list = File.read('wordsheet.txt')
-puts word_list
+word_list
+
+# make into an array seperate by /n
+# find words that are 5 - 12 in length
+# make that the array to sample from
+# initialize game with that
+list = word_list.split("\n")
+def check_length(word)
+    length = word.length
+    if length >= 5 && length <= 12
+        true
+    else
+        false
+    end
+end
+p valid_words = list.select { |word| check_length(word) }
