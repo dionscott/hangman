5565393ce4883ad05274771c5ce2ce493e7ef1ee
Dion Scott <dionanthonyscott@gmail.com>
2023-08-03 12:50:26 -0500

    Add function to check for valid input

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,24 @@
 class Player
     def initialize
     end
+
+    def get_input
+        input = gets.chomp.downcase
+    end
 end
 
 class Display
     def initialize
         puts "Let's play Hangman!"
+    end
+    # display the board
+    # ask the player for input
+    def display_player_input
+        puts "Input a letter."
+    end
+
+    def incorrect_input
+        puts "Incorrect input. Try again."
     end
 end
 
@@ -16,6 +29,8 @@ class Game
         @valid_words = list.select { |word| check_length(word) }
         @display = Display.new
         @player = Player.new
+        @correct_letters = []
+        @incorrect_letters = []
     end
     
     def check_length(word)
@@ -26,6 +41,34 @@ class Game
             false
         end
     end
+
+    def generate_word
+        @valid_words.sample
+    end
+
+    def is_string?(string)
+        if string.to_i.to_s == string
+            false
+        else
+            true
+        end
+    end
+
+    def valid_input?(string)
+        true if is_string?(string) && string.length == 1
+    end
+
+    def get_player_input
+        @display.display_player_input
+        input = @player.get_input
+        until valid_input?(input)
+            @display.incorrect_input
+            input = @player.get_input
+        end
+        puts "Correct input"
+    end
 end
 
 game = Game.new
+p game.generate_word
+p game.get_player_input
